d68b03fea145a6a5132392b1836d21db144a9638
martpendle <martpendle@gmail.com>
2016-07-03 17:04:13 +0100

    Resolved Issue with views

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 var express = require('express');
 
-var test = console.log('testing github works from webstorm')
-
 var app = express();
 
 var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
-
-app.set('views','./src/views');
-
-app.set('view engine', 'ejs');
+app.use(express.static('src/views'));
 
 app.get('/', function (req, res) {
     res.send('Hello World');
