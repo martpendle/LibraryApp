36b7a84cc2ddc8c90cef300b73fe94ac105cae00
martpendle <martpendle@gmail.com>
2016-07-03 16:31:53 +0100

    Checking in missing files

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 var express = require('express');
 
+var test = console.log('testing github works from webstorm')
+
 var app = express();
 
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 
-app.use(express.static('src/views'));
+app.set('views','./src/views');
+
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    res.send("Hello World");
+    res.send('Hello World');
 });
 
 app.get('/books', function (req, res) {
-    res.send("Hello Books");
+    res.send('Hello Books');
 });
 
 app.listen(port, function (err) {
